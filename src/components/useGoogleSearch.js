c4ab72e99e21a3e0355312806fbda6adbf2ac25f
Anita Yip <ceruleanox@gmail.com>
2021-12-04 14:55:16 -0500

    before creating static web app

--- src/components/useGoogleSearch.js
+++ src/components/useGoogleSearch.js
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react';
 import API_KEY from '../keys';
+//import { searchTerms } from '../data/searchTerms.json';
 
 const CONTEXT_KEY = "f8f6f5229fa039441";
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
+    let modSearch = ''
 
+    if (term.includes('computer scientist')) {
+        modSearch = 'Black female'.replaceAll(" ", "+");
+    }
     useEffect( () => {
         const fetchData = async() => {
             fetch(
-                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}+Black+female`
+                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}+${modSearch}`
             )
             .then(response => response.json())
             .then(result => {
@@ -21,5 +26,7 @@ const useGoogleSearch = (term) => {
 
     return { data }
 }
- 
+
+// ceruleanox
+
 export default useGoogleSearch
